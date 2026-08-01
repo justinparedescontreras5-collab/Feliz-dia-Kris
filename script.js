fdb706ca73cfa2e62e36826fd178057bc17f6d07
justinparedescontreras5-collab <justinparedescontreras5@gmail.com>
2026-08-01 11:00:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
+//======================================
+//      REPRODUCIR MÚSICA
+//======================================
 
+const musica = document.getElementById("musica");
+
+const botonMusica = document.getElementById("musicaBtn");
+
+let reproduciendo = false;
+
+botonMusica.addEventListener("click",()=>{
+
+    if(!reproduciendo){
+
+        musica.play();
+
+        botonMusica.innerHTML="⏸️ Pausar música";
+
+        reproduciendo=true;
+
+    }
+
+    else{
+
+        musica.pause();
+
+        botonMusica.innerHTML="🎵 Reproducir música";
+
+        reproduciendo=false;
+
+    }
+
+});
+
+//======================================
+//          CARTA
+//======================================
+
+const carta=document.getElementById("carta");
+
+const abrir=document.querySelector(".boton");
+
+const cerrar=document.getElementById("cerrarCarta");
+
+abrir.addEventListener("click",()=>{
+
+    carta.style.display="flex";
+
+});
+
+cerrar.addEventListener("click",()=>{
+
+    carta.style.display="none";
+
+});
